6d33d77b7872fc49e5b58094d02e530f78ebc836
ppinchuk <ppinchuk@nrel.gov>
2023-09-18 14:04:01 -0600

    Add `clobber` to collect CLI

--- gaps/cli/collect.py
+++ gaps/cli/collect.py
@@ -14,7 +14,12 @@ logger = logging.getLogger(__name__)
 
 
 def collect(
-    _out_path, _pattern, project_points=None, datasets=None, purge_chunks=False
+    _out_path,
+    _pattern,
+    project_points=None,
+    datasets=None,
+    purge_chunks=False,
+    clobber=True,
 ):
     """Run collection on local worker.
 
@@ -44,6 +49,11 @@ def collect(
         input files will **not** be removed if any of the datasets they
         contain have not been collected, regardless of the value of this
         input. By default, ``False``.
+    clobber : bool, optional
+        Flag to purge all collection output HDF5 files prior to running
+        the collection step if they exist on disk. This helps avoid any
+        surprising data byproducts when re-running the collection step
+        in a project directory. By default, ``True``.
 
     Returns
     -------
@@ -62,7 +72,7 @@ def collect(
     )
 
     datasets = _find_datasets(datasets, _pattern)
-    collector = Collector(_out_path, _pattern, project_points)
+    collector = Collector(_out_path, _pattern, project_points, clobber=clobber)
     for dataset_name in datasets:
         logger.debug("Collecting %r...", dataset_name)
         collector.collect(dataset_name)
