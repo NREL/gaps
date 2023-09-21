84f0a19d5a00b19d1ab7a7224cbca20b25f44980
ppinchuk <ppinchuk@nrel.gov>
2023-09-20 18:19:38 -0600

    Add keep thru option for reset-status (WIP)

--- gaps/cli/reset.py
+++ gaps/cli/reset.py
@@ -5,12 +5,14 @@ GAPs CLI template config generation command.
 import logging
 import shutil
 from pathlib import Path
+from warnings import warn
 
 import click
 
 from rex.utilities.loggers import init_logger
-from gaps.status import Status
+from gaps.status import Status, StatusOption
 from gaps.cli.command import _WrappedCommand
+from gaps.warnings import gapsHPCWarning
 
 
 logger = logging.getLogger(__name__)
@@ -18,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 # pylint: disable=redefined-builtin
 @click.pass_context
-def _reset_status(ctx, directory):
+def _reset_status(ctx, directory, force=False, keep_thru=None):
     """Filter configs and write to file based on type."""
     if ctx.obj.get("VERBOSE"):
         init_logger("gaps")
@@ -35,8 +37,27 @@ def _reset_status(ctx, directory):
             )
             continue
 
-        logger.info("Removing status info for directory %r", str(status_dir))
-        shutil.rmtree(status_file_dir)
+        status = Status(status_dir)
+        is_processing = (
+            status.as_df()
+            .job_status.isin({StatusOption.SUBMITTED, StatusOption.RUNNING})
+            .any()
+        )
+        if is_processing and not force:
+            msg = (
+                f"Found queued/running jobs in {str(status_dir)}. "
+                "Not resetting... (override this behavior with --force)"
+            )
+            warn(msg, gapsHPCWarning)
+            continue
+
+        if keep_thru:
+            pass
+        else:
+            logger.info(
+                "Removing status info for directory %r", str(status_dir)
+            )
+            shutil.rmtree(status_file_dir)
 
 
 def reset_command():
@@ -46,6 +67,19 @@ def reset_command():
             param_decls=["directory"],
             required=False,
             nargs=-1,
+        ),
+        click.Option(
+            param_decls=["--force", "-f"],
+            is_flag=True,
+            help="Force pipeline status reset even if jobs are queued/running",
+        ),
+        click.Option(
+            param_decls=["--keep-thru", "-k"],
+            multiple=False,
+            default=None,
+            help="Reset pipeline status up to the given command. This command "
+            "will remain completed, but the status of commands following it "
+            "will be reset completely.",
         ),
     ]
     return _WrappedCommand(
--- tests/cli/test_cli_reset.py
+++ tests/cli/test_cli_reset.py
@@ -7,15 +7,20 @@ from pathlib import Path
 
 import pytest
 
-from gaps.status import Status
+from gaps.status import (
+    Status,
+    StatusField,
+    StatusOption,
+    HardwareStatusRetriever,
+)
 from gaps.cli.reset import reset_command
 
 
 @pytest.mark.parametrize("add_dir", [True, False])
-def test_recursive_status(
+def test_reset_nominal(
     tmp_cwd, test_data_dir, cli_runner, add_dir, assert_message_was_logged
 ):
-    """Test the status command for recursive directories."""
+    """Test the reset command with and without directory input."""
 
     reset = reset_command()
 
@@ -44,6 +49,108 @@ def test_recursive_status(
     assert_message_was_logged("Removing status info for directory", "INFO")
     assert_message_was_logged(tmp_cwd.name, "INFO")
     assert not list(tmp_cwd.glob(Status.HIDDEN_SUB_DIR))
+
+
+def test_reset_force_option(
+    tmp_cwd, test_data_dir, cli_runner, assert_message_was_logged, monkeypatch
+):
+    """Test the force option for reset command."""
+
+    reset = reset_command()
+    shutil.copytree(
+        test_data_dir / "test_run" / Status.HIDDEN_SUB_DIR,
+        tmp_cwd / Status.HIDDEN_SUB_DIR,
+    )
+
+    monkeypatch.setattr(
+        HardwareStatusRetriever,
+        "__getitem__",
+        lambda *__, **___: "running",
+        raising=True,
+    )
+
+    assert list(tmp_cwd.glob(Status.HIDDEN_SUB_DIR)), list(tmp_cwd.glob("*"))
+
+    cli_runner.invoke(reset, obj={"VERBOSE": True})
+
+    assert_message_was_logged("Found queued/running jobs", "WARNING")
+    assert_message_was_logged(tmp_cwd.name, "WARNING")
+    assert_message_was_logged("Not resetting..", "WARNING", clear_records=True)
+    assert list(tmp_cwd.glob(Status.HIDDEN_SUB_DIR))
+
+    cli_runner.invoke(reset, ["--force"], obj={"VERBOSE": True})
+    assert_message_was_logged("Removing status info for directory", "INFO")
+    assert_message_was_logged(tmp_cwd.name, "INFO")
+    assert not list(tmp_cwd.glob(Status.HIDDEN_SUB_DIR))
+
+
+def test_reset_keep_thru_option(
+    tmp_cwd, test_data_dir, cli_runner, assert_message_was_logged, monkeypatch
+):
+    """Test the force option for reset command."""
+
+    reset = reset_command()
+    shutil.copytree(
+        test_data_dir / "test_run" / Status.HIDDEN_SUB_DIR,
+        tmp_cwd / Status.HIDDEN_SUB_DIR,
+    )
+
+    monkeypatch.setattr(
+        HardwareStatusRetriever,
+        "__getitem__",
+        lambda *__, **___: "running",
+        raising=True,
+    )
+
+    Status.mark_job_as_submitted(
+        tmp_cwd,
+        command="collect-run",
+        job_name="collect_job_0",
+        replace=False,
+        job_attrs={StatusField.JOB_STATUS: StatusOption.SUBMITTED},
+    )
+
+    Status.mark_job_as_submitted(
+        tmp_cwd,
+        command="collect-run",
+        job_name="collect_job_1",
+        replace=False,
+        job_attrs={StatusField.JOB_STATUS: StatusOption.SUBMITTED},
+    )
+
+    Status.make_single_job_file(
+        tmp_cwd,
+        command="collect-run",
+        job_name="collect_job_0",
+        attrs={StatusField.JOB_STATUS: StatusOption.SUCCESSFUL},
+    )
+    Status.make_single_job_file(
+        tmp_cwd,
+        command="collect-run",
+        job_name="collect_job_1",
+        attrs={StatusField.JOB_STATUS: StatusOption.SUCCESSFUL},
+    )
+
+    status_df = Status(tmp_cwd).as_df()
+    collect_status = status_df[status_df.index.str.startswith("collect")]
+    assert len(collect_status) == 2
+    assert (collect_status.job_status == StatusOption.SUCCESSFUL).all()
+
+    assert list(tmp_cwd.glob(Status.HIDDEN_SUB_DIR))
+    assert len(list((tmp_cwd / Status.HIDDEN_SUB_DIR).glob("*"))) > 1
+    assert len(list((tmp_cwd / Status.HIDDEN_SUB_DIR).glob("*collect*"))) == 2
+
+    # cli_runner.invoke(reset, obj={"VERBOSE": True})
+
+    # assert_message_was_logged("Found queued/running jobs", "WARNING")
+    # assert_message_was_logged(tmp_cwd.name, "WARNING")
+    # assert_message_was_logged("Not resetting..", "WARNING", clear_records=True)
+    # assert list(tmp_cwd.glob(Status.HIDDEN_SUB_DIR))
+
+    # cli_runner.invoke(reset, ["--force"], obj={"VERBOSE": True})
+    # assert_message_was_logged("Removing status info for directory", "INFO")
+    # assert_message_was_logged(tmp_cwd.name, "INFO")
+    # assert not list(tmp_cwd.glob(Status.HIDDEN_SUB_DIR))
 
 
 if __name__ == "__main__":
