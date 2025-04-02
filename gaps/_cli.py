f3fdd50b596f3e9c14637da5248ea5f4c268d61c
ppinchuk <pinchukpaul@gmail.com>
2025-04-01 18:08:28 -0600

    Disable linter

--- gaps/_cli.py
+++ gaps/_cli.py
@@ -6,7 +6,7 @@ GAPs CLI entry points.
 import click
 
 from gaps.cli.status import status_command
-from gaps._version import __version__
+from gaps._version import __version__  # pylint: disable=no-name-in-module
 
 
 @click.group()
